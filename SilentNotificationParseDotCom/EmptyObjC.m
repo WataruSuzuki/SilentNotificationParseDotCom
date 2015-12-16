9a907e5814ccd9baa5ad9869b986297af2ecbea7
WataruSuzuki <wataru0406@gmail.com>
2015-12-16 23:59:58 +0900

    add sample codes

---
+++ SilentNotificationParseDotCom/EmptyObjC.m
@@ -0,0 +1,9 @@
+//
+//  EmptyObjC.m
+//  SilentNotificationParseDotCom
+//
+//  Created by Wataru Suzuki on 2015/12/16.
+//  Copyright © 2015年 Wataru Suzuki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ SilentNotificationParseDotCom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SilentNotificationParseDotCom
+//
+//  Created by Wataru Suzuki on 2015/12/16.
+//  Copyright © 2015年 Wataru Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
